969eeb393e298a90e9a2250f3ede1fe23bd3f200
marinando <52049079+marinando@users.noreply.github.com>
2022-08-15 22:21:43 -0300

    Exercícios 15-08

---
+++ revisaoJava/src/ex3/Ex3.java
@@ -0,0 +1,37 @@
+//	3. Escreva um método responsável por calcular o abastecimento de um veículo. O método deve retornar o
+//	valor total a ser pago a partir do precoLitro e valorPagamento. Utilize wrapper classes do Java para os
+//	tipos usados e valide - utilizando controle de fluxo - caso valores nulos sejam fornecidos para precoLitro e
+//	valorPagamento.
+
+package ex3;
+
+import java.util.Scanner;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\nCalculadora de abastecimento\n\nInsira o valor do preço por litro do combustível:");
+		Scanner scanner = new Scanner(System.in);
+
+		Double precoLitro = Double.valueOf(scanner.nextLine());
+
+		System.out.println("Qual a quantidade de litros?\n");
+
+		Double quantidadeLitro = Double.valueOf(scanner.nextLine());
+		System.out.println("\nValor a pagar: " + calculaPreco(quantidadeLitro, precoLitro));
+
+	}
+
+	public static Double calculaPreco(Double qtdLitro, Double precoLitro) {
+
+		if (qtdLitro == null || precoLitro == null) {
+			System.out.println("Valor nulo inserido.");
+		} 
+		else {
+			return qtdLitro * precoLitro;
+		}
+		return null;
+	}
+
+}
