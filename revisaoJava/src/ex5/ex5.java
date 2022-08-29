a2f3ca98aa668530567b45780f719a167fed4ca3
Marina de Moraes Farias <marina.farias@hbsis.com.br>
2022-08-28 23:51:13 -0300

    Please ente the commit message for your changes. Lines starting

---
+++ revisaoJava/src/ex5/ex5.java
@@ -0,0 +1,17 @@
+package ex5;
+/*5. Descreva, com as suas palavras, a diferença conceitual entre “tipo primitivo” e “wrapper class”.
+ * 
+ * O tipo primitivo é o que conhecemos sem possibilidade de atribuição diferente daquilo a que se propõe. Ou seja, irá aceitar
+ * estritamente o tipo de dado que a variável for declarada, e não há a possibilidade de atribuição de métodos, com isso,
+ * atende apenas a sua funcionalidade.
+ * 
+ * Já as classes wrappers são capazes de fazer conversões em variáveis primitivas e também consegue os encapsular para serem 
+ * trabalhados como objetos.*/
+public class ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
